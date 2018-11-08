e4dbeb3f43d9676fb26cb1a17f4c4135d6ca7216
vivek638 <43683480+vivek638@users.noreply.github.com>
2018-11-08 21:32:28 +0530

    Add files via upload

---
+++ Migrations/201210280255140_InitialCreate.cs
@@ -0,0 +1,56 @@
+namespace OdeToFood.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserProfile",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        FavoriteRestaurant = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        City = c.String(),
+                        Country = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.RestaurantReviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Rating = c.Int(nullable: false),
+                        Body = c.String(maxLength: 1024),
+                        ReviewerName = c.String(),
+                        RestaurantId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Restaurants", t => t.RestaurantId, cascadeDelete: true)
+                .Index(t => t.RestaurantId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.RestaurantReviews", new[] { "RestaurantId" });
+            DropForeignKey("dbo.RestaurantReviews", "RestaurantId", "dbo.Restaurants");
+            DropTable("dbo.RestaurantReviews");
+            DropTable("dbo.Restaurants");
+            DropTable("dbo.UserProfile");
+        }
+    }
+}
